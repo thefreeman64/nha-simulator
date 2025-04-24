37b1a9138b838263c0c98c264a6121c028e80792
Quentin <quentinrenaud81@gmail.com>
2025-04-24 10:52:03 +0200

    Changed app code

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
+# Set page configuration
 st.set_page_config(page_title="National Handball Association", layout="centered")
+
+# Sidebar navigation (MUST come before the if-statement using `page`)
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to:", ["Home", "Basics of Handball", "Betting Simulator"])
 
 # Page Routing
 if page == "Home":
