98c7cd88033595ae46405ffcb7487f130f81d3bd
Quentin <quentinrenaud81@gmail.com>
2025-04-24 11:56:13 +0200

    Stuff added wont work

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="National Handball Association",
-    layout="centered",
-    initial_sidebar_state="expanded"
-)
+st.set_page_config(page_title="National Handball Association", layout="centered")
 
-st.markdown("<h1 style='text-align: center;'>Welcome to the National Handball Association!</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 48px;'>Welcome to the National Handball Association!</h1>", unsafe_allow_html=True)
 
-st.write("""
-The National Handball Association is a fictional sports league inspired by a love for handball and the NBA.
-Explore the Basics of Handball and try the Betting Simulator using the navigation on the left.
-""")
+st.markdown("""
+<div style='text-align: center; font-size: 18px;'>
+Explore the basics of handball, learn about the league, and try the betting simulator using the sidebar!
+</div>
+""", unsafe_allow_html=True)
